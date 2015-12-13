ea1ee2a9eafb25dd6b050805b394090cb684a3cc
William (B.J.) Snow Orvis <aetherknight@gmail.com>
2015-12-13 01:38:05 -0800

    Allowing dragon iteration to be specified via command line

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,8 @@ extern crate graphics;
 extern crate opengl_graphics;
 extern crate piston;
 
+use std::env;
+
 #[macro_use]
 pub mod common;
 pub mod dragon;
@@ -12,7 +14,13 @@ use dragon::DragonFractal;
 use glwindow::{WindowHandler};
 
 fn main() {
+    let args = env::args().collect::<Vec<String>>();
+    if args.len() <= 1 {
+        panic!("You must provide an iteration number");
+    }
+    let iterations = args.get(1).unwrap().parse::<u64>().unwrap();
+
     let window = WindowHandler::new();
-    let dr = DragonFractal::new(17).unwrap();
+    let dr = DragonFractal::new(iterations).unwrap();
     window.run(&dr);
 }
