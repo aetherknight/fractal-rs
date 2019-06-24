d24e426dce69ee505134df99cccee98f4759efe9
William (B.J.) Snow Orvis <aetherknight@gmail.com>
2019-06-23 17:37:42 -0700

    Rework webpack.config.js to support multiple modes, and use different dist directories for dev vs prod

--- fractal-wasm/www/webpack.config.js
+++ fractal-wasm/www/webpack.config.js
@@ -3,15 +3,17 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 const webpack = require("webpack");
 const WasmPackPlugin = require("@wasm-tool/wasm-pack-plugin");
 
-module.exports = {
-  entry: "./index.js",
-  output: {
-    path: path.resolve(__dirname, "dist"),
-    filename: "index.js"
-  },
-  mode: "development",
-  plugins: [
-    new HtmlWebpackPlugin({ template: "index.html" }),
-    new WasmPackPlugin({ crateDirectory: path.resolve(__dirname, "..") })
-  ]
-};
+let config = (module.exports = (env, argv) => {
+  return {
+    entry: "./index.js",
+    output: {
+      path: path.resolve(__dirname, "dist", argv.mode),
+      filename: "index.js"
+    },
+    // mode: "development",
+    plugins: [
+      new HtmlWebpackPlugin({ template: "index.html" }),
+      new WasmPackPlugin({ crateDirectory: path.resolve(__dirname, "..") })
+    ]
+  };
+});
