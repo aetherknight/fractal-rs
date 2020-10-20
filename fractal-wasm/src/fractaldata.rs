34f550b3b0a9b135a0dbebdc8522117338221198
William (B.J.) Snow Orvis <aetherknight@gmail.com>
2020-10-19 21:31:57 -0700

    Replace macros with generic functions in fractal-wasm

--- fractal-wasm/src/fractaldata.rs
+++ fractal-wasm/src/fractaldata.rs
@@ -1,5 +1,6 @@
 use fractal_lib::chaosgame::barnsleyfern;
 use fractal_lib::chaosgame::sierpinski;
+use fractal_lib::chaosgame::ChaosGameMoveIterator;
 use fractal_lib::curves::cesaro;
 use fractal_lib::curves::cesarotri;
 use fractal_lib::curves::dragon;
@@ -8,7 +9,9 @@ use fractal_lib::curves::levyccurve;
 use fractal_lib::curves::terdragon;
 use fractal_lib::escapetime::burningship::{BurningMandel, BurningShip, RoadRunner};
 use fractal_lib::escapetime::mandelbrot::Mandelbrot;
+use fractal_lib::escapetime::EscapeTime;
 use fractal_lib::lindenmayer::LindenmayerSystemTurtleProgram;
+use fractal_lib::turtle::TurtleProgram;
 use fractal_lib::FractalCategory;
 use fractal_lib::SelectedFractal;
 use wasm_bindgen::prelude::JsValue;
@@ -47,18 +50,81 @@ impl SelectedFractalExt for SelectedFractal {
         config: &FractalConfig,
     ) -> Box<dyn FractalAnimation> {
         match self {
-            SelectedFractal::BarnsleyFern => Box::new(animated_barnsleyfern(canvas)),
-            SelectedFractal::BurningMandel => Box::new(animated_burningmandel(canvas, config)),
-            SelectedFractal::BurningShip => Box::new(animated_burningship(canvas, config)),
-            SelectedFractal::Cesaro => Box::new(animated_cesaro(canvas, config)),
-            SelectedFractal::CesaroTri => Box::new(animated_cesarotri(canvas, config)),
-            SelectedFractal::Dragon => Box::new(animated_dragon(canvas, config)),
-            SelectedFractal::KochCurve => Box::new(animated_kochcurve(canvas, config)),
-            SelectedFractal::LevyCCurve => Box::new(animated_levyccurve(canvas, config)),
-            SelectedFractal::Mandelbrot => Box::new(animated_mandelbrot(canvas, config)),
-            SelectedFractal::RoadRunner => Box::new(animated_roadrunner(canvas, config)),
-            SelectedFractal::Sierpinski => Box::new(animated_sierpinski(canvas)),
-            SelectedFractal::TerDragon => Box::new(animated_terdragon(canvas, config)),
+            SelectedFractal::BarnsleyFern => Box::new(animated_chaos_game(
+                canvas,
+                &|| {
+                    barnsleyfern::BarnsleyFern::new(
+                        &barnsleyfern::REFERENCE_TRANSFORMS,
+                        &barnsleyfern::REFERENCE_WEIGHTS,
+                    )
+                },
+                self.name(),
+            )),
+            SelectedFractal::BurningMandel => Box::new(animated_escape_time(
+                canvas,
+                config,
+                &BurningMandel::new,
+                self.name(),
+            )),
+            SelectedFractal::BurningShip => Box::new(animated_escape_time(
+                canvas,
+                config,
+                &BurningShip::new,
+                self.name(),
+            )),
+            SelectedFractal::Cesaro => Box::new(animated_turtle(
+                canvas,
+                config,
+                &LindenmayerSystemTurtleProgram::build(cesaro::CesaroFractal::new),
+                self.name(),
+            )),
+            SelectedFractal::CesaroTri => Box::new(animated_turtle(
+                canvas,
+                config,
+                &LindenmayerSystemTurtleProgram::build(cesarotri::CesaroTriFractal::new),
+                self.name(),
+            )),
+            SelectedFractal::Dragon => Box::new(animated_turtle(
+                canvas,
+                config,
+                &dragon::DragonFractal::new,
+                self.name(),
+            )),
+            SelectedFractal::KochCurve => Box::new(animated_turtle(
+                canvas,
+                config,
+                &LindenmayerSystemTurtleProgram::build(kochcurve::KochCurve::new),
+                self.name(),
+            )),
+            SelectedFractal::LevyCCurve => Box::new(animated_turtle(
+                canvas,
+                config,
+                &LindenmayerSystemTurtleProgram::build(levyccurve::LevyCCurve::new),
+                self.name(),
+            )),
+            SelectedFractal::Mandelbrot => Box::new(animated_escape_time(
+                canvas,
+                config,
+                &Mandelbrot::new,
+                self.name(),
+            )),
+            SelectedFractal::RoadRunner => Box::new(animated_escape_time(
+                canvas,
+                config,
+                &RoadRunner::new,
+                self.name(),
+            )),
+            SelectedFractal::Sierpinski => Box::new(animated_chaos_game(
+                canvas,
+                &sierpinski::SierpinskiChaosGame::new,
+                self.name(),
+            )),
+            SelectedFractal::TerDragon => Box::new(animated_turtle(
+                canvas,
+                config,
+                &LindenmayerSystemTurtleProgram::build(terdragon::TerdragonFractal::new),
+                self.name(),
+            )),
         }
     }
 }
@@ -94,184 +160,73 @@ impl FractalConfig {
     }
 }
 
-/// Macro that generates a function for constructing a TurtleAnimation for a particular kind of
-/// turtle-based curve.
-///
-/// It takes a name identifier, a colon, and then expression that should evaluate to a
-/// TurtleProgram. The expression may use `iteration` in order to configure the TurtleProgram.
-///
-/// For example:
-/// ```rust,ignore
-/// animated_turtle!(dragon: dragon::DragonFractal::new(iteration));
-/// ```
-///
-/// Will create a function with signature:
-///
-/// ```rust, ignore
-/// pub fn animated_dragon(canvas: &HtmlCanvaselement, config: &FractalConfig) -> turtle::TurtleAnimation;
-/// ```
-///
-/// It will blank out the screen, start the TurtleAnimation, and then return it. The caller may
-/// then call `draw_one_frame` on future frames/ticks to update/animate the canvas.
-macro_rules! animated_turtle {
-    ($name:ident: $expr:expr) => {
-        // Paste is needed to concatenate render_ and the name of the fractal. Rust's own macros
-        // don't provide a good way to do this.
-        paste::item! {
-            /// Blanks the canvas and constructs a `TurtleAnimation` that represents the given
-            /// curve, and then returns the `TurtleAnimation`, allowing the call to render
-            /// additional frames.
-            ///
-            /// The iteration specifies which iteration of the TurtleProgram it will draw.
-            pub fn [<animated_ $name>] (
-                canvas: &HtmlCanvasElement,
-                config: &FractalConfig,
-            ) -> turtle::TurtleAnimation {
-                match config {
-                    FractalConfig::TurtleCurveConfig{iteration: iteration} => {
-                        log::debug!("Starting animation {}", stringify!($name));
-                        let ctx = JsValue::from(canvas.get_context("2d").unwrap().unwrap())
-                            .dyn_into::<CanvasRenderingContext2d>()
-                            .unwrap();
-                        ctx.clear_rect(0.0, 0.0, canvas.width().into(), canvas.height().into());
+fn animated_turtle<E, F>(
+    canvas: &HtmlCanvasElement,
+    config: &FractalConfig,
+    ctor: &F,
+    name: &'static str,
+) -> turtle::TurtleAnimation
+where
+    E: TurtleProgram + 'static,
+    F: Fn(u64) -> E,
+{
+    match config {
+        FractalConfig::TurtleCurveConfig { iteration } => {
+            log::debug!("Starting animation {}", name);
+            let ctx = JsValue::from(canvas.get_context("2d").unwrap().unwrap())
+                .dyn_into::<CanvasRenderingContext2d>()
+                .unwrap();
+            ctx.clear_rect(0.0, 0.0, canvas.width().into(), canvas.height().into());
 
-                        let program = $expr;
-                        turtle::TurtleAnimation::new(ctx, &(program(*iteration)))
-                    },
-                    _ => { panic!("{} needs a TurtleCurveConfig", stringify!($name)) },
-                }
-            }
+            turtle::TurtleAnimation::new(ctx, &(ctor(*iteration)))
         }
-    };
+        _ => panic!("{} needs a TurtleCurveConfig", stringify!($name)),
+    }
 }
 
-animated_turtle!(
-    cesaro: |iteration| {
-        LindenmayerSystemTurtleProgram::new(cesaro::CesaroFractal::new(iteration))
-    }
-);
-animated_turtle!(
-    cesarotri: |iteration| {
-        LindenmayerSystemTurtleProgram::new(cesarotri::CesaroTriFractal::new(iteration))
-    }
-);
-animated_turtle!(
-    dragon: |iteration| {
-        dragon::DragonFractal::new(iteration)
-    }
-);
-animated_turtle!(
-    kochcurve: |iteration| {
-        LindenmayerSystemTurtleProgram::new(kochcurve::KochCurve::new(iteration))
-    }
-);
-animated_turtle!(
-    levyccurve: |iteration| {
-        LindenmayerSystemTurtleProgram::new(levyccurve::LevyCCurve::new(iteration))
-    }
-);
-animated_turtle!(
-    terdragon: |iteration| {
-        LindenmayerSystemTurtleProgram::new(terdragon::TerdragonFractal::new(iteration))
-    }
-);
+fn animated_chaos_game<E, F>(
+    canvas: &HtmlCanvasElement,
+    ctor: &F,
+    name: &'static str,
+) -> chaosgame::ChaosGameAnimation
+where
+    E: ChaosGameMoveIterator + 'static,
+    F: Fn() -> E,
+{
+    log::debug!("Starting animation {}", name);
+    let ctx = JsValue::from(canvas.get_context("2d").unwrap().unwrap())
+        .dyn_into::<CanvasRenderingContext2d>()
+        .unwrap();
 
-/// Macro that generates a function for constructing (and starting) a ChaosGameAnimation for a
-/// particular kind of ChaosGame.
-///
-/// It takes a name identifier, a colon, and then expression that should evaluate to a
-/// a ChaosGame.
-///
-/// For example:
-/// ```rust,ignore
-/// animated_chaos_game!(sierpinski: sierpinski::SierpinskiChaosGame::new());
-/// ```
-///
-/// Will create a function with signature:
-///
-/// ```rust,ignore
-/// #[wasm_bindgen]
-/// pub fn animated_sierpinski(canvas: &HtmlCanvaselement) -> ChaosGameAnimation;
-/// ```
-///
-/// It will blank out the screen, start the ChaosGameAnimation, and then return it. The caller may
-/// then call `draw_one_frame` on future frames/ticks to update/animate the canvas.
-macro_rules! animated_chaos_game {
-    ($name:ident: $expr:expr) => {
-        // Paste is needed to concatenate render_ and the name of the fractal. Rust's own macros
-        // don't provide a good way to do this.
-        paste::item! {
-            pub fn [<animated_ $name>] (
-                canvas: &HtmlCanvasElement
-            ) -> chaosgame::ChaosGameAnimation {
-                log::debug!("Starting animation {}", stringify!($name));
-                let ctx = JsValue::from(canvas.get_context("2d").unwrap().unwrap())
-                    .dyn_into::<CanvasRenderingContext2d>()
-                    .unwrap();
+    ctx.clear_rect(0.0, 0.0, canvas.width().into(), canvas.height().into());
 
-                ctx.clear_rect(0.0, 0.0, canvas.width().into(), canvas.height().into());
-
-                chaosgame::ChaosGameAnimation::new(ctx, Box::new($expr))
-            }
-        }
-    };
+    chaosgame::ChaosGameAnimation::new(ctx, Box::new(ctor()))
 }
 
-animated_chaos_game!(
-    barnsleyfern:
-        barnsleyfern::BarnsleyFern::new(
-            &barnsleyfern::REFERENCE_TRANSFORMS,
-            &barnsleyfern::REFERENCE_WEIGHTS,
-        )
-);
+fn animated_escape_time<E, F>(
+    canvas: &HtmlCanvasElement,
+    config: &FractalConfig,
+    ctor: &F,
+    name: &'static str,
+) -> escapetime::EscapeTimeAnimation
+where
+    E: EscapeTime + 'static,
+    F: Fn(u64, u64) -> E,
+{
+    match config {
+        FractalConfig::EscapeTimeConfig {
+            max_iterations,
+            power,
+        } => {
+            log::debug!("Starting animation {}", name);
+            let ctx = JsValue::from(canvas.get_context("2d").unwrap().unwrap())
+                .dyn_into::<CanvasRenderingContext2d>()
+                .unwrap();
 
-animated_chaos_game!(sierpinski: sierpinski::SierpinskiChaosGame::new());
+            ctx.clear_rect(0.0, 0.0, canvas.width().into(), canvas.height().into());
 
-macro_rules! animated_escape_time {
-    ($name:ident: $expr:expr) => {
-        // Paste is needed to concatenate render_ and the name of the fractal. Rust's own macros
-        // don't provide a good way to do this.
-        paste::item! {
-            pub fn [<animated_ $name>] (
-                canvas: &HtmlCanvasElement,
-                config: &FractalConfig,
-            ) -> escapetime::EscapeTimeAnimation {
-                match config {
-                    FractalConfig::EscapeTimeConfig{max_iterations: max_iterations, power: power} => {
-                        log::debug!("Starting animation {}", stringify!($name));
-                        let ctx = JsValue::from(canvas.get_context("2d").unwrap().unwrap())
-                            .dyn_into::<CanvasRenderingContext2d>()
-                            .unwrap();
-
-                        ctx.clear_rect(0.0, 0.0, canvas.width().into(), canvas.height().into());
-
-                        let fractal = $expr;
-                        escapetime::EscapeTimeAnimation::new(ctx, Box::new(fractal(*max_iterations, *power)))
-                    },
-                    _ => { panic!("{} needs a EscapeTimeconfig", stringify!($name)) },
-                }
-            }
+            escapetime::EscapeTimeAnimation::new(ctx, Box::new(ctor(*max_iterations, *power)))
         }
-    };
+        _ => panic!("{} needs a EscapeTimeconfig", stringify!($name)),
+    }
 }
-
-animated_escape_time!(
-    burningmandel: |max_iterations, power| {
-        BurningMandel::new(max_iterations, power)
-    }
-);
-animated_escape_time!(
-    burningship: |max_iterations, power| {
-        BurningShip::new(max_iterations, power)
-    }
-);
-animated_escape_time!(
-    mandelbrot: |max_iterations, power| {
-        Mandelbrot::new(max_iterations, power)
-    }
-);
-animated_escape_time!(
-    roadrunner: |max_iterations, power| {
-        RoadRunner::new(max_iterations, power)
-    }
-);
